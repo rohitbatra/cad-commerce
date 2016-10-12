cbffe6db3e389043d681f5e0685a4982ef045a50
Rohit Batra <rohit@rohitbatra.in>
2016-10-12 18:08:14 +0530

    Installed Opencart
    Change the config.php & admin/config.php params to set this up on a
    different server.
    Also there is db_schema folder with latest schema

---
+++ config.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://cad.ergoasia.com/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://cad.ergoasia.com/');
+
+// DIR
+define('DIR_APPLICATION', 'C:/xampp/htdocs/cad-commerce/cad-commerce/catalog/');
+define('DIR_SYSTEM', 'C:/xampp/htdocs/cad-commerce/cad-commerce/system/');
+define('DIR_IMAGE', 'C:/xampp/htdocs/cad-commerce/cad-commerce/image/');
+define('DIR_LANGUAGE', 'C:/xampp/htdocs/cad-commerce/cad-commerce/catalog/language/');
+define('DIR_TEMPLATE', 'C:/xampp/htdocs/cad-commerce/cad-commerce/catalog/view/theme/');
+define('DIR_CONFIG', 'C:/xampp/htdocs/cad-commerce/cad-commerce/system/config/');
+define('DIR_CACHE', 'C:/xampp/htdocs/cad-commerce/cad-commerce/system/storage/cache/');
+define('DIR_DOWNLOAD', 'C:/xampp/htdocs/cad-commerce/cad-commerce/system/storage/download/');
+define('DIR_LOGS', 'C:/xampp/htdocs/cad-commerce/cad-commerce/system/storage/logs/');
+define('DIR_MODIFICATION', 'C:/xampp/htdocs/cad-commerce/cad-commerce/system/storage/modification/');
+define('DIR_UPLOAD', 'C:/xampp/htdocs/cad-commerce/cad-commerce/system/storage/upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'cad_commerce');
+define('DB_PORT', '3306');
+define('DB_PREFIX', '');
