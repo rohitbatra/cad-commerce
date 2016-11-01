6ac72385e975fe1c68247ccec5e17e1ee0aa96e5
Rohit Batra <rohit@rohitbatra.in>
2016-11-01 19:51:16 +0530

    config_files renamed were causing issues on multiple locations.

---
+++ admin/config_windows_git.php
@@ -0,0 +1,31 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://cad.ergoasia.com/admin/');
+define('HTTP_CATALOG', 'http://cad.ergoasia.com/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://cad.ergoasia.com/admin/');
+define('HTTPS_CATALOG', 'http://cad.ergoasia.com/');
+
+// DIR
+define('DIR_APPLICATION', 'C:/xampp/htdocs/cad-commerce/cad-commerce/admin/');
+define('DIR_SYSTEM', 'C:/xampp/htdocs/cad-commerce/cad-commerce/system/');
+define('DIR_IMAGE', 'C:/xampp/htdocs/cad-commerce/cad-commerce/image/');
+define('DIR_LANGUAGE', 'C:/xampp/htdocs/cad-commerce/cad-commerce/admin/language/');
+define('DIR_TEMPLATE', 'C:/xampp/htdocs/cad-commerce/cad-commerce/admin/view/template/');
+define('DIR_CONFIG', 'C:/xampp/htdocs/cad-commerce/cad-commerce/system/config/');
+define('DIR_CACHE', 'C:/xampp/htdocs/cad-commerce/cad-commerce/system/storage/cache/');
+define('DIR_DOWNLOAD', 'C:/xampp/htdocs/cad-commerce/cad-commerce/system/storage/download/');
+define('DIR_LOGS', 'C:/xampp/htdocs/cad-commerce/cad-commerce/system/storage/logs/');
+define('DIR_MODIFICATION', 'C:/xampp/htdocs/cad-commerce/cad-commerce/system/storage/modification/');
+define('DIR_UPLOAD', 'C:/xampp/htdocs/cad-commerce/cad-commerce/system/storage/upload/');
+define('DIR_CATALOG', 'C:/xampp/htdocs/cad-commerce/cad-commerce/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'cad_commerce');
+define('DB_PORT', '3306');
+define('DB_PREFIX', '');
--- config.php
+++ config.php
@@ -1,28 +1,28 @@
 <?php
 // HTTP
-define('HTTP_SERVER', 'http://cad.ergoasia.com/');
+    define('HTTP_SERVER', 'http://sandbox.dev/cad-commerce/');
 
 // HTTPS
-define('HTTPS_SERVER', 'http://cad.ergoasia.com/');
+    define('HTTPS_SERVER', 'http://sandbox.dev/cad-commerce/');
 
 // DIR
-define('DIR_APPLICATION', 'C:/xampp/htdocs/cad-commerce/cad-commerce/catalog/');
-define('DIR_SYSTEM', 'C:/xampp/htdocs/cad-commerce/cad-commerce/system/');
-define('DIR_IMAGE', 'C:/xampp/htdocs/cad-commerce/cad-commerce/image/');
-define('DIR_LANGUAGE', 'C:/xampp/htdocs/cad-commerce/cad-commerce/catalog/language/');
-define('DIR_TEMPLATE', 'C:/xampp/htdocs/cad-commerce/cad-commerce/catalog/view/theme/');
-define('DIR_CONFIG', 'C:/xampp/htdocs/cad-commerce/cad-commerce/system/config/');
-define('DIR_CACHE', 'C:/xampp/htdocs/cad-commerce/cad-commerce/system/storage/cache/');
-define('DIR_DOWNLOAD', 'C:/xampp/htdocs/cad-commerce/cad-commerce/system/storage/download/');
-define('DIR_LOGS', 'C:/xampp/htdocs/cad-commerce/cad-commerce/system/storage/logs/');
-define('DIR_MODIFICATION', 'C:/xampp/htdocs/cad-commerce/cad-commerce/system/storage/modification/');
-define('DIR_UPLOAD', 'C:/xampp/htdocs/cad-commerce/cad-commerce/system/storage/upload/');
+    define('DIR_APPLICATION', '/Users/rohit/Desktop/sandbox/cad-commerce/catalog/');
+    define('DIR_SYSTEM', '/Users/rohit/Desktop/sandbox/cad-commerce/system/');
+    define('DIR_IMAGE', '/Users/rohit/Desktop/sandbox/cad-commerce/image/');
+    define('DIR_LANGUAGE', '/Users/rohit/Desktop/sandbox/cad-commerce/catalog/language/');
+    define('DIR_TEMPLATE', '/Users/rohit/Desktop/sandbox/cad-commerce/catalog/view/theme/');
+    define('DIR_CONFIG', '/Users/rohit/Desktop/sandbox/cad-commerce/system/config/');
+    define('DIR_CACHE', '/Users/rohit/Desktop/sandbox/cad-commerce/system/storage/cache/');
+    define('DIR_DOWNLOAD', '/Users/rohit/Desktop/sandbox/cad-commerce/system/storage/download/');
+    define('DIR_LOGS', '/Users/rohit/Desktop/sandbox/cad-commerce/system/storage/logs/');
+    define('DIR_MODIFICATION', '/Users/rohit/Desktop/sandbox/cad-commerce/system/storage/modification/');
+    define('DIR_UPLOAD', '/Users/rohit/Desktop/sandbox/cad-commerce/system/storage/upload/');
 
 // DB
-define('DB_DRIVER', 'mysqli');
-define('DB_HOSTNAME', 'localhost');
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', '');
-define('DB_DATABASE', 'cad_commerce');
-define('DB_PORT', '3306');
-define('DB_PREFIX', '');
+    define('DB_DRIVER', 'mysqli');
+    define('DB_HOSTNAME', 'localhost');
+    define('DB_USERNAME', 'root');
+    define('DB_PASSWORD', 'hackme');
+    define('DB_DATABASE', 'cad_commerce');
+    define('DB_PORT', '3306');
+    define('DB_PREFIX', '');
